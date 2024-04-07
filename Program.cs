36f98828193c67041c84b83691721d01837f1cfa
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2024-04-07 21:09:42 +0200

    Add code

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Colore;
+using Colore.Effects.Keyboard;
+using Colore.Data;
+using SharpHook;
+using System.Collections.Concurrent;
+
+namespace Kbg.LightMyRazer;
+
+internal class Program
+{
+    static ConcurrentQueue<ushort> KeyQueue = new ConcurrentQueue<ushort>();
+    static CancellationTokenSource cts = new CancellationTokenSource();
+    static CustomKeyboardEffect effect = new CustomKeyboardEffect(Color.Black);
+
+    static async Task Main(string[] args)
+    {
+        var appInfo = new AppInfo("Light my Razer",
+            "Light chroma when using other keyboards",
+            "Kasper B. Graversen",
+            "bolig48@gmail.com",
+            Category.Application);
+        using IChroma chroma = await ColoreProvider.CreateRestAsync(appInfo);
+        Thread.Sleep(150);
+
+
+        using var hook = new TaskPoolGlobalHook();
+        hook.KeyPressed += OnKeyPressed;
+        hook.RunAsync();
+
+        var kl = new KeyListen(KeyQueue, effect);
+        Task.Run(() => kl.KeyListener(cts.Token), cts.Token);
+
+
+        var fader = new RazerFadeEffect(effect);
+
+        while (true)
+        {
+            bool changed = fader.FadeAllKeys();
+
+            if (changed)
+            {
+                await chroma.Keyboard.SetCustomAsync(effect);
+            }
+
+            Thread.Sleep(100);
+        }
+
+        cts.Cancel();
+
+    }
+
+    /// <summary>
+    /// just enqueue presses so the keyhandler is never a bottleneck to the OS which may slay our process if we become slow
+    /// </summary>
+    private static void OnKeyPressed(object? sender, KeyboardHookEventArgs e) => KeyQueue.Enqueue(e.Data.RawCode);
+
+}
---
+++ RazerFadeEffect.cs
@@ -0,0 +1,173 @@
+﻿using Colore.Effects.Keyboard;
+using Colore.Data;
+
+namespace Kbg.LightMyRazer;
+
+class RazerFadeEffect(CustomKeyboardEffect effect)
+{
+    static Color[] Colors =
+   {
+        Color.Blue,
+        Color.Green,
+        Color.HotPink,
+        Color.Orange,
+        Color.Pink,
+        Color.Purple,
+        Color.Red,
+        Color.Yellow,
+    };
+
+    bool fader(Key k)
+    {
+        Color color1 = effect[k];
+
+        if (color1 == Color.White)
+        {
+            effect[k] = Colors[Random.Shared.Next(Colors.Length)];
+            return true;
+        }
+
+        if (color1 != Color.Black)
+        {
+            Color ndarker = new Color(
+                             (byte)Math.Max(0, color1.R - 5),
+                             (byte)Math.Max(0, color1.G - 5),
+                             (byte)Math.Max(0, color1.B - 5));
+            effect[k] = ndarker;
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public bool FadeAllKeys()
+    {
+        bool change = false;
+        change |= fader(Key.Logo);
+        change |= fader(Key.Escape);
+        change |= fader(Key.F1);
+        change |= fader(Key.F2);
+        change |= fader(Key.F3);
+        change |= fader(Key.F4);
+        change |= fader(Key.F5);
+        change |= fader(Key.F6);
+        change |= fader(Key.F7);
+        change |= fader(Key.F8);
+        change |= fader(Key.F9);
+        change |= fader(Key.F10);
+        change |= fader(Key.F11);
+        change |= fader(Key.F12);
+        change |= fader(Key.D1);
+        change |= fader(Key.D2);
+        change |= fader(Key.D3);
+        change |= fader(Key.D4);
+        change |= fader(Key.D5);
+        change |= fader(Key.D6);
+        change |= fader(Key.D7);
+        change |= fader(Key.D8);
+        change |= fader(Key.D9);
+        change |= fader(Key.D0);
+        change |= fader(Key.A);
+        change |= fader(Key.B);
+        change |= fader(Key.C);
+        change |= fader(Key.D);
+        change |= fader(Key.E);
+        change |= fader(Key.F);
+        change |= fader(Key.G);
+        change |= fader(Key.H);
+        change |= fader(Key.I);
+        change |= fader(Key.J);
+        change |= fader(Key.K);
+        change |= fader(Key.L);
+        change |= fader(Key.M);
+        change |= fader(Key.N);
+        change |= fader(Key.O);
+        change |= fader(Key.P);
+        change |= fader(Key.Q);
+        change |= fader(Key.R);
+        change |= fader(Key.S);
+        change |= fader(Key.T);
+        change |= fader(Key.U);
+        change |= fader(Key.V);
+        change |= fader(Key.W);
+        change |= fader(Key.X);
+        change |= fader(Key.Y);
+        change |= fader(Key.Z);
+        change |= fader(Key.NumLock);
+        change |= fader(Key.Num0);
+        change |= fader(Key.Num1);
+        change |= fader(Key.Num2);
+        change |= fader(Key.Num3);
+        change |= fader(Key.Num4);
+        change |= fader(Key.Num5);
+        change |= fader(Key.Num6);
+        change |= fader(Key.Num7);
+        change |= fader(Key.Num8);
+        change |= fader(Key.Num9);
+        change |= fader(Key.NumDivide);
+        change |= fader(Key.NumMultiply);
+        change |= fader(Key.NumSubtract);
+        change |= fader(Key.NumAdd);
+        change |= fader(Key.NumEnter);
+        change |= fader(Key.NumDecimal);
+        change |= fader(Key.PrintScreen);
+        change |= fader(Key.Scroll);
+        change |= fader(Key.Pause);
+        change |= fader(Key.Insert);
+        change |= fader(Key.Home);
+        change |= fader(Key.PageUp);
+        change |= fader(Key.Delete);
+        change |= fader(Key.End);
+        change |= fader(Key.PageDown);
+        change |= fader(Key.Up);
+        change |= fader(Key.Left);
+        change |= fader(Key.Down);
+        change |= fader(Key.Right);
+        change |= fader(Key.Tab);
+        change |= fader(Key.CapsLock);
+        change |= fader(Key.Backspace);
+        change |= fader(Key.Enter);
+        change |= fader(Key.LeftControl);
+        change |= fader(Key.LeftWindows);
+        change |= fader(Key.LeftAlt);
+        change |= fader(Key.Space);
+        change |= fader(Key.RightAlt);
+        change |= fader(Key.Function);
+        change |= fader(Key.RightMenu);
+        change |= fader(Key.RightControl);
+        change |= fader(Key.LeftShift);
+        change |= fader(Key.RightShift);
+        change |= fader(Key.Macro1);
+        change |= fader(Key.Macro2);
+        change |= fader(Key.Macro3);
+        change |= fader(Key.Macro4);
+        change |= fader(Key.Macro5);
+        change |= fader(Key.OemTilde);
+        change |= fader(Key.OemMinus);
+        change |= fader(Key.OemEquals);
+        change |= fader(Key.OemLeftBracket);
+        change |= fader(Key.OemRightBracket);
+        change |= fader(Key.OemBackslash);
+        change |= fader(Key.OemSemicolon);
+        change |= fader(Key.OemApostrophe);
+        change |= fader(Key.OemComma);
+        change |= fader(Key.OemPeriod);
+        change |= fader(Key.OemSlash);
+        change |= fader(Key.EurPound);
+        change |= fader(Key.EurBackslash);
+        change |= fader(Key.JpnYen);
+        change |= fader(Key.JpnSlash);
+        change |= fader(Key.Jpn3);
+        change |= fader(Key.Jpn4);
+        change |= fader(Key.Jpn5);
+        change |= fader(Key.KorPipe);
+        change |= fader(Key.Kor2);
+        change |= fader(Key.Kor3);
+        change |= fader(Key.Kor4);
+        change |= fader(Key.Kor5);
+        change |= fader(Key.Kor6);
+        change |= fader(Key.Kor7);
+        return change;
+    }
+}
